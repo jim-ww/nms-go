2cea2295effe335ca7c8c4f16230cd460495d21e
jim-ww <jim.w2610@proton.me>
2024-11-23 00:18:48 +0200

    add help command to shell.nix

--- shell.nix
+++ shell.nix
@@ -11,9 +11,17 @@ mkShell {
   ];
 
   shellHook = ''
-    alias test='go test ./...'
-    alias test-verbose='go test -v -cover ./...'
     alias build-css='tailwindcss build -o web/static/tailwind.css'
     alias run='build-css && air'
+    alias test='go test ./...'
+    alias test-verbose='go test -v -cover ./...'
+    alias help='echo "$HELP"'
+    export HELP='
+      $ run - rebuilds c and starts server with air
+      $ build-css - rebuild css with tailwind
+      $ test - run all tests
+      $ test-verbose - run tests verbose + with coverage
+      $ help - print this message'
+    echo "$HELP"
   '';
 }
