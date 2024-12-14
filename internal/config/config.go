58a083b8fabdaf1fefa442d00ca1291d6ca69fce
jim-ww <jim.w2610@proton.me>
2024-12-14 06:40:53 +0200

    update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,34 +1,35 @@
 package config
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type JWTTokenConfig struct {
-	Secret             string        `json:"secret"`
-	ExpirationDuration time.Duration `json:"expiration_duration"` // TODO
+	Secret             string        `yaml:"secret" json:"secret" env:"SECRET"`
+	ExpirationDuration time.Duration `yaml:"expiration_duration" json:"expiration_duration" env:"EXPIRATION_DURATION"`
 }
 
 type Config struct {
-	Env            string `json:"env" env:"ENV" env-default:"local"` // local, dev, prod
-	StoragePath    string `json:"storage_path" env:"STORAGE_PATH" env-default:"/storage/sqlite/nms.db"`
-	HTTPServer     `json:"http_server"`
-	JWTTokenConfig *JWTTokenConfig `json:"jwt-token"`
+	Env            string          `yaml:"env" json:"env" env:"ENV" env-default:"local"` // local, dev, prod
+	StoragePath    string          `yaml:"storage_path" json:"storage_path" env:"STORAGE_PATH" env-default:"/storage/sqlite/storage.db"`
+	HTTPServer     HTTPServer      `yaml:"http_server" json:"http_server"`
+	JWTTokenConfig *JWTTokenConfig `yaml:"jwt_token" json:"jwt_token"`
 }
 
 type HTTPServer struct {
-	Address string `json:"address"` // TODO default values?
-	// Timeout     time.Duration `json:"timeout" env-default:"4s"`  // TODO implement duration values reader?
-	// IdleTimeout time.Duration `json:"idle_timeout" env-default:"60s"`
+	Address     string        `json:"address" yaml:"address" env:"adress" env-default:"localhost:8080"`
+	Timeout     time.Duration `json:"timeout" yaml:"timeout" env:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `json:"idle_timeout" yaml:"idle_timeout" env:"idle_timeout" env-default:"60s"`
 }
 
 func MustLoad() *Config {
 	configPath := os.Getenv("CONFIG_PATH")
-	defaultConfigPath := "config/local.json"
+	defaultConfigPath := "configs/local.yml"
 
 	if configPath == "" {
 		fmt.Println("CONFIG_PATH is not set, using default config:", defaultConfigPath)
@@ -41,13 +42,8 @@ func MustLoad() *Config {
 
 	var cfg Config
 
-	jsonFile, err := os.ReadFile(configPath)
-	if err != nil {
-		log.Fatalf("Error reading file: %v", err)
-	}
-
-	if err := json.Unmarshal(jsonFile, &cfg); err != nil {
-		log.Fatalf("failed to parse config at %s: %v", configPath, err)
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("failed to parse config at %s, err: %s", configPath, err.Error())
 	}
 
 	return &cfg
